636043e48f60a4dd4cedc11b800bf6a69aaa4a94
svnoliveira <srkuran@gmail.com>
2024-01-22 11:56:18 -0300

    feat: ajustado e testado rota de registro de pontos

--- registros_de_ponto/serializers.py
+++ registros_de_ponto/serializers.py
@@ -11,11 +11,10 @@ class RegistroSerializer(serializers.ModelSerializer):
             "id",
             "entrada",
             "saida",
-            "horas"
+            "horas",
             "justificativa",
             "user",
         ]
-        depth = 1
         extra_kwargs = {
             "user": {'read_only': True},
             "horas": {'read_only': True},
@@ -26,11 +25,11 @@ class RegistroSerializer(serializers.ModelSerializer):
         data_dict = {}
         for key, value in validated_data.items():
             if key == "entrada":
-                horas["entrada"] = dt.datetime(value)
-                data_dict[key] = dt.datetime(value)
+                horas["entrada"] = value
+                data_dict[key] = value
             elif key == "saida":
-                horas["saida"] = dt.datetime(value)
-                data_dict[key] = dt.datetime(value)
+                horas["saida"] = value
+                data_dict[key] = value
             else:
                 data_dict[key] = value
 
@@ -39,7 +38,7 @@ class RegistroSerializer(serializers.ModelSerializer):
         seconds %= 3600
         minutes = seconds // 60
         seconds %= 60
-        time = dt.time(hours, minutes, seconds)
+        time = dt.time(int(hours), int(minutes), int(seconds))
         data_dict["horas"] = time
 
         instance = Registro_de_ponto.objects.create(**data_dict)
@@ -51,11 +50,11 @@ class RegistroSerializer(serializers.ModelSerializer):
         horas = {}
         for key, value in validated_data.items():
             if key == "entrada":
-                horas["entrada"] = dt.datetime(value)
-                setattr(instance, key, dt.datetime(value))
+                horas["entrada"] = value
+                setattr(instance, key, value)
             elif key == "saida":
-                horas["saida"] = dt.datetime(value)
-                setattr(instance, key, dt.datetime(value))
+                horas["saida"] = value
+                setattr(instance, key, value)
             else:
                 setattr(instance, key, value)
         seconds = (horas["saida"] - horas["entrada"]).total_seconds()
@@ -63,7 +62,7 @@ class RegistroSerializer(serializers.ModelSerializer):
         seconds %= 3600
         minutes = seconds // 60
         seconds %= 60
-        time = dt.time(hours, minutes, seconds)
+        time = dt.time(int(hours), int(minutes), int(seconds))
         setattr(instance, "horas", time)
         instance.save()
 
--- registros_de_ponto/views.py
+++ registros_de_ponto/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import get_object_or_404
 from .serializers import RegistroSerializer
+from django.db.models.query import QuerySet
 from .models import Registro_de_ponto
 from users.models import User
 from rest_framework.generics import (
+    ListAPIView,
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
 from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class RegistroListView(ListAPIView):
+    authentication_classes = [JWTAuthentication]
+
+    serializer_class = RegistroSerializer
+    queryset = Registro_de_ponto.objects.all()
 
 
 class RegistroListCreateView(ListCreateAPIView):
@@ -15,6 +24,20 @@ class RegistroListCreateView(ListCreateAPIView):
 
     serializer_class = RegistroSerializer
     queryset = Registro_de_ponto.objects.all()
+    lookup_url_kwarg = "user_id"
+
+    def get_queryset(self):
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method."
+            % self.__class__.__name__
+        )
+
+        queryset = self.queryset
+        if isinstance(queryset, QuerySet):
+            # Ensure queryset is re-evaluated on each request.
+            queryset = queryset.filter(user=self.kwargs["user_id"])
+        return queryset
 
     def perform_create(self, serializer):
         user = get_object_or_404(User, pk=self.kwargs["user_id"])
